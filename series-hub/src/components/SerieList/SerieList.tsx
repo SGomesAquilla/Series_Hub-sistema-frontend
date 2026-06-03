616492aaf953c36c177fb60e63ebf0f72381aa9f
AquillaSG <aquillagomes@hotmail.com>
2026-06-03 20:01:25 -0300

    adicionando metodo para editar serie

--- series-hub/src/components/SerieList/SerieList.tsx
+++ series-hub/src/components/SerieList/SerieList.tsx
@@ -3,10 +3,12 @@ import { type Serie } from '../SerieForm/SerieForm';
 interface SerieListProps {
   series: Serie[];
   onExcluir: (id: number) => void;
+  onEditar: (serie: Serie) => void;
 }
 
 export default function SerieList({
   series,
+  onEditar,
   onExcluir,
 }: SerieListProps) {
   return (
@@ -18,6 +20,12 @@ export default function SerieList({
           <li key={serie.id}>
             {serie.titulo} - {serie.temporadas} temporadas -
             {serie.diretor}
+
+            <button
+              onClick={() => onEditar(serie)}
+            >
+              Editar
+            </button>
 
             <button
               onClick={() => onExcluir(serie.id)}
--- series-hub/src/pages/Series/Series.tsx
+++ series-hub/src/pages/Series/Series.tsx
@@ -1,35 +1,43 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import {type Serie } from '../../components/SerieForm/SerieForm';
 import SerieList from '../../components/SerieList/SerieList';
 import { mockSeries } from '../../data/mockSeries';
 
 export default function Series() {
-  const [series, setSeries] = useState<Serie[]>(() => {
-    const seriesSalvas = localStorage.getItem('series');
+    const navigate = useNavigate();
 
-    return seriesSalvas
-        ? JSON.parse(seriesSalvas)
-        : mockSeries;
-  });
+    const [series, setSeries] = useState<Serie[]>(() => {
+        const seriesSalvas = localStorage.getItem('series');
 
-  function excluirSerie(id: number) {
-    const novasSeries = series.filter(
-        (serie) => serie.id !== id
+        return seriesSalvas
+            ? JSON.parse(seriesSalvas)
+            : mockSeries;
+    });
+
+    function editarSerie(serie: Serie) {
+        navigate(`/editar/${serie.id}`);
+    }
+
+    function excluirSerie(id: number) {
+        const novasSeries = series.filter(
+            (serie) => serie.id !== id
+        );
+
+        setSeries(novasSeries);
+
+        localStorage.setItem(
+            'series',
+            JSON.stringify(novasSeries)
+        );
+    }
+
+    return (
+        <SerieList
+        series={series}
+        onEditar={editarSerie}
+        onExcluir={excluirSerie}
+        />
     );
-
-    setSeries(novasSeries);
-
-    localStorage.setItem(
-        'series',
-        JSON.stringify(novasSeries)
-    );
-  }
-
-  return (
-    <SerieList
-      series={series}
-      onExcluir={excluirSerie}
-    />
-  );
 }
