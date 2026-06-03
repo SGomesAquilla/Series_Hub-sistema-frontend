dc7ac9ad7add245e96c07cf0822784bb9d0494cd
AquillaSG <aquillagomes@hotmail.com>
2026-06-03 15:03:25 -0300

    configurando o router

---
+++ series-hub/src/routes/AppRoutes.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Home from '../pages/Home/Home';
+import Cadastro from '../pages/Cadastro/Cadastro';
+import Series from '../pages/Series/Series';
+
+export default function AppRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/series" element={<Series />} />
+        </Routes>
+    );
+}
